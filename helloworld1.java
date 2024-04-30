6a78232c16c41603f4406feb3dbcc161614d4e3e
roshnirai-moogle <137078050+roshnirai-moogle@users.noreply.github.com>
2024-04-30 17:06:15 +0530

    Update helloworld1.java

--- helloworld1.java
+++ helloworld1.java
@@ -1,5 +1,6 @@
 class Sample{
     public static void main(String args[]){
         System.out.println("hii..my name is roshni rai. this is my jenkins practical");
+        System.out.println("hello, welcome to the world.");
     }
 }
